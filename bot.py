34001273a5eee61f8935f1634c0bcc658ac2cea2
sksaif72-rgb <sksaif72@gmail.com>
2026-03-06 13:02:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
+import psycopg2
+import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL)
+cursor = conn.cursor()
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        ["اشتراك"],
+        ["مدرب"]
+    ]
+
+    reply = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+    await update.message.reply_text(
+        "اهلا بك في بوت التخمين",
+        reply_markup=reply
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
